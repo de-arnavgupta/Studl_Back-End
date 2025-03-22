6e157871504fbca271b3e8d455c3c3cc0be32198
vivek-0509 <viveksolanki0509@gmail.com>
2025-03-22 20:36:13 +0530

    Implemented JwtFilter and Handles exceptions in it

--- src/main/java/de/arnav/studl/security/service/MyUserDetailService.java
+++ src/main/java/de/arnav/studl/security/service/MyUserDetailService.java
@@ -3,11 +3,25 @@ package de.arnav.studl.security.service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MyUserDetailService implements UserDetailsService {
+
+    private final UserJpaRepository userJpaRepository;
+
+    public MyUserDetailService(UserJpaRepository userJpaRepository){
+        this.userJpaRepository = userJpaRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user=userJpaRepository.findByUsername(username);
+
+        if(user==null){
+            throw new UsernameNotFoundException(username);
+        }
+
+        return new UserPrincipal(user);
     }
 }
--- src/main/java/de/arnav/studl/security/service/UserPrincipal.java
+++ src/main/java/de/arnav/studl/security/service/UserPrincipal.java
@@ -1,44 +1,53 @@
 package de.arnav.studl.security.service;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
-public class UserPrinciple implements UserDetails {
+public class UserPrincipal implements UserDetails {
+
+    private final User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        // Convert the user's role into a GrantedAuthority
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().toUpperCase()));
     }
 
     @Override
     public String getPassword() {
-        return "";
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return user.getUsername();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return true;
     }
 }
