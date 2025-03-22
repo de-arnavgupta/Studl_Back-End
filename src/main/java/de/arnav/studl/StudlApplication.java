f094be45996797c0b617af7a5e6f1c4247400d50
noir <noir@odin.com>
2025-03-22 15:28:24 +0530

    intial set-up of the project with postgres, docker

---
+++ src/main/java/de/arnav/studl/StudlApplication.java
@@ -0,0 +1,13 @@
+package de.arnav.studl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/arnav/studl/StudlApplicationTests.java
@@ -0,0 +1,13 @@
+package de.arnav.studl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
