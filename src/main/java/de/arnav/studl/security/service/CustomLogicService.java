41baa22d22682c9542b0bb0935f13d58649aa3a9
vivek-0509 <viveksolanki0509@gmail.com>
2025-03-23 20:59:14 +0530

    Made some changed in CustomLogicService class

--- src/main/java/de/arnav/studl/security/service/CustomLogicService.java
+++ src/main/java/de/arnav/studl/security/service/CustomLogicService.java
@@ -1,5 +1,6 @@
 package de.arnav.studl.security.service;
 
+import de.arnav.studl.security.model.Role;
 import org.springframework.stereotype.Service;
 import java.util.HashSet;
 import java.util.Set;
@@ -8,25 +9,28 @@ import java.util.Set;
 public class CustomLogicService {
 
     private final ExcelEmailChecker excelEmailChecker;
+    private final RoleType roleType;
 
-    public CustomLogicService(ExcelEmailChecker excelEmailChecker) {
+    public CustomLogicService(ExcelEmailChecker excelEmailChecker,RoleType roleType) {
         this.excelEmailChecker = excelEmailChecker;
+        this.roleType = roleType;
     }
 
-    public Set<String> assignRoles(String email) {
-        Set<String> roles = new HashSet<>();
+    public Set<RoleType> assignRoles(String email) {
+        Set<RoleType> roles = new HashSet<>();
 
         try {
             String domain = email.split("@")[1];
 
             if (domain.equals("scaler.com")) {
                 if (excelEmailChecker.isEmailInExcel(email)) {
-                    roles.add("ROLE_ADMIN"); // Assign admin role if found in the Excel sheet
+                    roles.add(RoleType.ROLE_ADMIN); // Assign admin role if found in the Excel sheet
                 }
             }
             else if (domain.equals("sst.scaler.com")) {
-                roles.add("ROLE_STUDENT"); // Assign student role
+                roles.add(RoleType.ROLE_STUDENT); // Assign student role
             }
+
         } catch (Exception e) {
             throw new RuntimeException("Error while assigning roles", e);
         }
