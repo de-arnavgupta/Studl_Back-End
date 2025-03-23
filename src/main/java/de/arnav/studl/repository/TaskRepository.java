384b8d361a80c48e690ac92b4260c3618f77e9c1
noir <noir@odin.com>
2025-03-24 02:09:14 +0530

    completed TaskRepository

---
+++ src/main/java/de/arnav/studl/repository/TaskRepository.java
@@ -0,0 +1,17 @@
+package de.arnav.studl.repository;
+
+import de.arnav.studl.model.Task;
+import de.arnav.studl.model.enums.TaskPriority;
+import de.arnav.studl.model.enums.TaskStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findAllByStatus(TaskStatus status);
+    List<Task> findAllByPriority(TaskPriority priority);
+    List<Task> findAllByAssignee_UserId(Long userId);
+    List<Task> findAllByReporter_UserId(Long userId);
+    List<Task> findAllByDueAtBefore(LocalDateTime dueAt);
+    List<Task> findAllByLabels_LabelId(Long labelId);
+}
