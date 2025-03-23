4adc2a9038521f8b6fc26d0fba69102d9181de6b
noir <noir@odin.com>
2025-03-24 02:33:05 +0530

    completed LabelSummaryDto

---
+++ src/main/java/de/arnav/studl/dto/label/LabelSummaryDto.java
@@ -0,0 +1,12 @@
+package de.arnav.studl.dto.label;
+
+public class LabelSummaryDto {
+    private Long labelId;
+    private String name;
+
+    // Getters and setters
+    public Long getLabelId() { return labelId; }
+    public void setLabelId(Long labelId) { this.labelId = labelId; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
