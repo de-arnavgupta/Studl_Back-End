306925334a0651a57b20d839410d1a5c485780e1
Trinity2206 <srinidhi2206@gmail.com>
2025-03-24 19:51:50 +0530

    feat(DTOs): Created appropriate DTOs that was missing and their methods.

--- src/main/java/de/arnav/studl/dto/userRoleDto/UserRoleCreateDto.java
+++ src/main/java/de/arnav/studl/dto/userRoleDto/UserRoleCreateDto.java
@@ -1,4 +1,26 @@
 package de.arnav.studl.dto.userRoleDto;
 
+import de.arnav.studl.model.RoleType;
+
 public class UserRoleCreateDto {
+
+    private RoleType roleType;
+    private Long userId;
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
 }
--- src/main/java/de/arnav/studl/dto/userRoleDto/UserRoleResponseDto.java
+++ src/main/java/de/arnav/studl/dto/userRoleDto/UserRoleResponseDto.java
@@ -1,4 +1,34 @@
 package de.arnav.studl.dto.userRoleDto;
 
+import de.arnav.studl.model.RoleType;
+
 public class UserRoleResponseDto {
+
+    private Long userRoleId;
+    private RoleType roleType;
+    private Long userId;
+
+    public Long getUserRoleId() {
+        return userRoleId;
+    }
+
+    public void setUserRoleId(Long userRoleId) {
+        this.userRoleId = userRoleId;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 }
