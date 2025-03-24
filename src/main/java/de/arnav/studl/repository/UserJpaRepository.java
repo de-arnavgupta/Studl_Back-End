f9354166f67c2d0f356faf6d8607926e2d805b1c
Om Malviya <ommalviya1810@gmail.com>
2025-03-25 02:18:49 +0530

    Removed UserJpaRepository from auth/model

--- src/main/java/de/arnav/studl/repository/UserJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package de.arnav.studl.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import de.arnav.studl.model.User;
-
-public interface UserJpaRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
